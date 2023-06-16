7b3b8523679bc95bf1f99be28a83fd7bf2826ff7
sw59 <sw59@njit.edu>
2023-06-15 20:11:59 -0400

    myTest.py

--- myTest.py
+++ myTest.py
@@ -1,4 +1,21 @@
-count = 0
-for i in range(1,101):
-    count+=i
-print(count)
+def fibonacci(n):
+    if n <= 0:
+        return "输入错误，请输入一个正整数"
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        a, b = 0, 1
+        for i in range(2, n):
+            a, b = b, a + b
+        return b
+
+
+assert fibonacci(1) == 0, "Error: fibonacci(1) should return 0"
+assert fibonacci(2) == 1, "Error: fibonacci(2) should return 1"
+assert fibonacci(3) == 1, "Error: fibonacci(3) should return 1"
+assert fibonacci(4) == 2, "Error: fibonacci(4) should return 2"
+assert fibonacci(5) == 3, "Error: fibonacci(5) should return 3"
+assert fibonacci(6) == 5, "Error: fibonacci(6) should return 5"
+assert fibonacci(7) == 8, "Error: fibonacci(7) should return 8"
